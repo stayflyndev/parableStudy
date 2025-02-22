0c680e0ea367027aeed188400a57fae328d85919
Tori Adams <aretee64@gmail.com>
2025-02-21 22:25:48 -0500

    complete but will add updates

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY; // Used for fetching parables
 
 app.use(cors({
-    origin: "https://parablestudy.onrender.com",
+    origin: "https://parablestudystudy.onrender.com",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true
